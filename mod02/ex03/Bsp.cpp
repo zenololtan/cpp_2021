799126cbb8f6b289fdcc576154de617b6fccdbe8
Zeno Tan <ztan@f0r4s7.codam.nl>
2021-10-26 14:35:43 +0200

    updated bsp to fail points on the border

--- mod02/ex03/Bsp.cpp
+++ mod02/ex03/Bsp.cpp
@@ -1,6 +1,6 @@
 #include "Point.hpp"
 
-Fixed	cross_product(Fixed const Ax, Fixed const Ay, Fixed const Bx, Fixed const By)
+Fixed	dot_product(Fixed const Ax, Fixed const Ay, Fixed const Bx, Fixed const By)
 {
 	Fixed	mp1;
 	Fixed	mp2;
@@ -12,19 +12,19 @@ Fixed	cross_product(Fixed const Ax, Fixed const Ay, Fixed const Bx, Fixed const 
 
 bool	same_side(Point const p1, Point const a, Point const b, Point const c)
 {
-	Fixed	dp;
-	Fixed	cp1;
-	Fixed	cp2;
-	Fixed	cb1;
-	Fixed	cb2;
+	Fixed	mp;
+	Fixed	dp1;
+	Fixed	dp2;
+	Fixed	db1;
+	Fixed	db2;
 
-	cb1 = c.getX() - b.getX();
-	cb2 = c.getY() - b.getY();
-	cp1 = cross_product(cb1, p1.getX() - b.getX(), cb2, p1.getY() - b.getY());
-	cp2 = cross_product(cb1, a.getX() - b.getX(), cb2, a.getY() - b.getY());
+	db1 = c.getX() - b.getX();
+	db2 = c.getY() - b.getY();
+	dp1 = dot_product(db1, p1.getX() - b.getX(), db2, p1.getY() - b.getY());
+	dp2 = dot_product(db1, a.getX() - b.getX(), db2, a.getY() - b.getY());
 
-	dp = cp1 * cp2;
-	if (dp >= 0)
+	mp = dp1 * dp2;
+	if (mp > 0)
 		return true;
 	else
 		return false;
@@ -32,7 +32,6 @@ bool	same_side(Point const p1, Point const a, Point const b, Point const c)
 
 bool    bsp(Point const a, Point const b, Point const c, Point const pt)
 {
-	std::cout << "b after passing it as arg: " << b << std::endl;
 	if (same_side(pt, a, b, c) && same_side(pt, b, a, c) && same_side(pt, c, a, b))
 		return true;
 	else
